a798b9b14a8a8510d77216ecdb2071f7deff3823
ofctarun <narayanashettitarun01@gmail.com>
2025-12-27 10:15:45 +0530

    backend errors fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -49,7 +49,7 @@ const PORT = process.env.PORT || 5000;
 
 initDb().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is currently running on port ${PORT}`);
   });
 });
 
